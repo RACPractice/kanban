a68b890e7e85b02ba2f88d9ba2cdf62d8c6deef5
Mohan Radhakrishnan <rmohankumar@gmail.com>
2012-12-18 15:54:28 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kanban::Application.config.secret_token = 'e95c23ff16a10474467c69482322ff878f9d4105540e43cae07235746860282858997afdc6e5fad6ab031b3a3456129da036ca19b883828c64869081e9d8829e'
