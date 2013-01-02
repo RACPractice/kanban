9c5fff04b154a2d90a2dbeae9e0b979d422581f4
Mohan Radhakrishnan <rmohankumar@gmail.com>
2013-01-02 15:00:00 -0500

    Created account model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121220170930) do
+ActiveRecord::Schema.define(:version => 20130102194437) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
