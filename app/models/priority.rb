cc00900a700b629e474b61cdc96a34e3719da523
Vladimir Repcak <Vladimir.Repcak@Ness.Com>
2013-01-03 13:43:02 -0500

    added basic validations for the priority and work_type

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,5 +1,6 @@
 class Priority < ActiveRecord::Base
   attr_accessible :name
+  validates :name, :presence => true, :uniqueness => true
 
 	# association
 	belongs_to :work_item
--- app/models/work_type.rb
+++ app/models/work_type.rb
@@ -1,3 +1,4 @@
 class WorkType < ActiveRecord::Base
    attr_accessible :name
+   validates :name, :presence => true, :uniqueness => true
 end
