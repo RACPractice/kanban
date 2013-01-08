edfc124b59c55197041cb359a2e38bf929a83a88
Mohan Radhakrishnan <rmohankumar@gmail.com>
2013-01-08 13:44:32 -0500

    Adding tasks form and workitemassociation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,13 @@
 class Task < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name, :description, :work_item_id
+	before_create :create_slug
 
 	# association
 	belongs_to :work_item
+
+	# Generates the Slug field
+	def create_slug
+		self.slug = self.name.parameterize
+	end
+
 end
