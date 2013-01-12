62d7bdb7553f1edb93c20afbcd6416333efb122e
Mohan Radhakrishnan <rmohankumar@ness.com>
2013-01-11 20:33:06 -0500

    Cleaning up the forms using bootstrap and removed topnav

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Kanban::Application.routes.draw do
-  resources :tasks
-
-
-  resources :work_items
-
-
-  resources :projects
-
-
-  resources :accounts
-
-
-  resources :work_types
-	resources :steps
-  resources :priorities
 
   get "home/index"
 
+  root :to => 'home#index'
+
+  resources :tasks
+
+  resources :work_items
+
+  resources :projects
+
+  resources :accounts
+
+  resources :work_types
+
+	resources :steps
+
+  resources :priorities
+
 	devise_for :users, :controllers => { :registrations => "users/registrations" }
 
-  root :to => 'home#index'
 end
