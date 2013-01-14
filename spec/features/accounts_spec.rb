d6f2ffe4605fa7d590a28f0c1351048219107b64
iulian <iulian@foghorn.(none)>
2013-01-15 01:15:30 +0200

    The joy of TDD part 1: wrote a spec that catches a bug in the New account form

---
+++ spec/features/accounts_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "Accounts management" do
+
+  before do
+    # Log in
+    @user = FactoryGirl.create(:user)
+    visit '/users/sign_in'
+    fill_in 'Username or email', :with => @user.email
+    fill_in 'Password', :with => @user.password
+    click_button "Sign in"
+  end
+
+  it "should create a new owned account" do
+    visit new_account_path
+    fill_in 'Name', :with => 'AnAccount'
+    click_button "Create Account"
+    page.should have_content 'AnAccount'
+  end
+end
